b34cc833f30e217d6ebcc047cf8ac9c8221b1d2a
Reko Meriö <K9260@student.jamk.fi>
2020-01-26 15:27:13 +0200

    Dialog and table for food intolerances

--- src/components/FoodIntoleranceList.js
+++ src/components/FoodIntoleranceList.js
@@ -1,15 +1,30 @@
 import React, { useState, useEffect } from "react";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import makeStyles from "@material-ui/core/styles/makeStyles";
 import { firestore } from "../firebase";
+import { connect } from "react-redux";
+import { setLoading } from "../redux/actions";
+import { Link } from "react-router-dom";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import EditIcon from "@material-ui/icons/Edit";
+import Fab from "@material-ui/core/Fab";
+import { formatDistance } from "date-fns";
+import { fi } from "date-fns/locale";
+import Dialog from "./Dialog";
+import Typography from "@material-ui/core/Typography";
 
-const FoodIntoleranceList = () => {
-    const classes = useStyles();
+const FoodIntoleranceList = ({ setLoading }) => {
     const [intolerances, setIntolerances] = useState([]);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [dialogContent, setDialogContent] = useState({});
 
     useEffect(() => {
+        setLoading(true);
+
         firestore
             .collection("intolerances")
             .get()
@@ -20,41 +35,79 @@ const FoodIntoleranceList = () => {
                 });
                 setIntolerances(arr);
             })
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false));
     }, []);
 
+    const openDialog = i => () => {
+        const intolerance = intolerances[i];
+        const render = intolerance.text.split("\n").map(text => (
+            <Typography key={text} variant="body1">
+                {text}
+            </Typography>
+        ));
+        setDialogContent({ title: intolerance.id.slice(0, 20), render });
+        setIsDialogOpen(true);
+    };
+
     return (
-        <div className={classes.root}>
-            <Grid container spacing={2}>
-                {intolerances.map(intolerance => (
-                    <Grid key={intolerance.id} item xs>
-                        <Paper className={classes.paper}>
-                            <Typography variant="subtitle2">
-                                {intolerance.createdBy}
-                            </Typography>
-                            {intolerance.text.split("\n").map((text, i) => (
-                                <Typography key={i} variant="body1">
-                                    {text}
-                                </Typography>
-                            ))}
-                        </Paper>
-                    </Grid>
-                ))}
-            </Grid>
+        <div>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">Nro</TableCell>
+                            <TableCell align="left">Käyttäjä</TableCell>
+                            <TableCell align="left">Teksti</TableCell>
+                            <TableCell align="left">Id</TableCell>
+                            <TableCell align="left">Luotu</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {intolerances.map((intolerance, i) => (
+                            <TableRow key={intolerance.id}>
+                                <TableCell component="th" scope="row">
+                                    {i + 1}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    <Link
+                                        style={{ color: "black" }}
+                                        to={"/user/" + intolerance.createdBy + "/edit"}
+                                    >
+                                        <Fab size="small" color="primary">
+                                            <EditIcon />
+                                        </Fab>
+                                    </Link>
+                                </TableCell>
+                                <TableCell
+                                    component="th"
+                                    scope="row"
+                                    onClick={openDialog(i)}
+                                    style={{ cursor: "pointer" }}
+                                >
+                                    {intolerance.text.length > 20
+                                        ? intolerance.text.slice(0, 20) + "..."
+                                        : intolerance.text}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {intolerance.id.slice(0, 10) + "..."}
+                                </TableCell>
+                                <TableCell component="th" scope="row">
+                                    {formatDistance(intolerance.createdAt, new Date(), {
+                                        locale: fi
+                                    })}{" "}
+                                    sitten
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Dialog setOpen={setIsDialogOpen} open={isDialogOpen} title={dialogContent.title}>
+                {dialogContent.render}
+            </Dialog>
         </div>
     );
 };
 
-export default FoodIntoleranceList;
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-        margin: theme.spacing(2)
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: "center",
-        color: theme.palette.text.secondary
-    }
-}));
+export default connect(null, { setLoading })(FoodIntoleranceList);
