67d8f1e6205806b74cb4e1a93bfd819f3f90c974
Reko Meriö <K9260@student.jamk.fi>
2020-02-06 12:49:22 +0200

    Add  marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,25 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import MAPS_API_KEY from "../api_keys";
+import { MAPS_API_KEY } from "../api_keys";
 
 const Map = ({ height, width }) => {
+    const renderMarkers = (map, maps) => {
+        const marker = new maps.Marker({
+            position: { lat: 62.14478, lng: 25.467761 },
+            title: "The place to be",
+            map
+        });
+        return marker;
+    };
+
     return (
         <div style={{ height: height || "100%", width: width || "100%" }}>
             <GoogleMapReact
                 bootstrapURLKeys={MAPS_API_KEY}
-                defaultCenter={{ lat: 60, lng: 30 }}
-                defaultZoom={11}
+                defaultCenter={{ lat: 62.14478, lng: 25.467761 }}
+                defaultZoom={12}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
             ></GoogleMapReact>
         </div>
     );
